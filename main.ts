58fb56471025fca44ecd05867b2731cec6965b42
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2025-09-19 13:22:11 -0400

    When A is pressed the temperature is shown

--- main.ts
+++ main.ts
@@ -1,8 +1,24 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Sultan
+ * Created on: Sep 2025
+ * This program shows the temperature when A button pressed
 */
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
 
-basic.showString('Hello, World!')
+
+
+ // When A button is pressed the temapture whill be showm
+let Tempature: number
+
+input.onButtonPressed(Button.A, function() {
+    basic.clearScreen()
+ basic.showString('The temperature is ' + input.temperature())
+basic.clearScreen()
+basic.pause(1000)
+})
+
+
+
