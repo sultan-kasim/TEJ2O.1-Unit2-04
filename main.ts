d4e945d2464b3c11280ff1645ae61ed110c93d4f
sultan-kasim <sultan.kasim@stu.ocsb.ca>
2025-09-26 12:34:26 -0400

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,22 +2,24 @@
  *
  * Created by: Sultan
  * Created on: Sep 2025
- * This program shows the temperature when A button pressed
+ * This program shows the temperature when A button pressed.
 */
+
 basic.clearScreen()
 basic.showIcon(IconNames.Happy)
 basic.pause(1000)
 
-
- // When A button is pressed the temperature whill be showm
+// when A button is pressed the temperature will be shown
+let temperature:number
  
 input.onButtonPressed(Button.A, function() {
-basic.clearScreen()
-basic.showString('The temperature is ' + input.temperature())
+    // shows temperature on mircobit
+    basic.clearScreen()
+    basic.showString('The temperature is ' + input.temperature())
+    temperature = input.temperature()
+    basic.showNumber(temperature)
 
-basic.clearScreen()
-basic.pause(1000)
+    // resets mircobit screen
+    basic.clearScreen()
+    basic.showIcon(IconNames.Happy)
 })
-
-
-
